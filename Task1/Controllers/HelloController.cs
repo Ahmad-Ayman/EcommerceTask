ea5d4a3a2120e83155722693dd0eba16d98c044d
Ahmed Ayman Abd El-Moneim <ahmed.ayman1708@gmail.com>
2018-11-02 20:04:30 +0200

    Initial

---
+++ Task1/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public String Index()
+        {
+            return "Welcome to Main Screen for Hello Controller";
+        }
+        public String Welcome()
+        {
+            return "Welcome Page";
+        }
+    }
+}
